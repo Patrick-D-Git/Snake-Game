00062aca61f9c9757d0733db3f59224aa47c5f56
Patrick D <patrick.montejo@gmail.com>
2024-02-21 22:25:26 -0500

    added food.py file

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 screen = Screen()
@@ -10,6 +11,8 @@ screen.tracer(0)
 
 
 snake = Snake()
+food = Food()
+
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -23,6 +26,9 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
--- snake.py
+++ snake.py
@@ -8,6 +8,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def create_snake(self):
         for position in STARTING_POSITION:
@@ -22,24 +23,24 @@ class Snake:
             new_x = self.segments[seg_num - 1].xcor()
             new_y = self.segments[seg_num - 1].ycor()
             self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
 
-        if self.segments[0].heading() != 270.0:
-            self.segments[0].setheading(90)
+        if self.head.heading() != 270.0:
+            self.head.setheading(90)
 
     def down(self):
 
-        if self.segments[0].heading() != 90.0:
-            self.segments[0].setheading(270)
+        if self.head.heading() != 90.0:
+            self.head.setheading(270)
 
     def left(self):
 
-        if self.segments[0].heading() != 0.0:
-            self.segments[0].setheading(180)
+        if self.head.heading() != 0.0:
+            self.head.setheading(180)
 
     def right(self):
 
-        if self.segments[0].heading() != 180.0:
-            self.segments[0].setheading(0)
+        if self.head.heading() != 180.0:
+            self.head.setheading(0)
