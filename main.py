025768cc463c20fc9c401d04eddf520f91f68596
Patrick D <patrick.montejo@gmail.com>
2024-02-22 21:47:23 -0500

    added wall collision condition and game over function

--- main.py
+++ main.py
@@ -10,11 +10,9 @@ screen.bgcolor("black")
 screen.title("My Snake Game")
 screen.tracer(0)
 
-
 snake = Snake()
 food = Food()
 scoreboard = Scoreboard()
-
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -29,10 +27,15 @@ while game_is_on:
     time.sleep(0.1)
     snake.move()
 
+    #  collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
         scoreboard.add_score()
         scoreboard.refresh()
+
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
 
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -1,4 +1,6 @@
 from turtle import Turtle
+ALIGNMENT = "center"
+FONT = ('Courier', 24, 'normal')
 
 
 class Scoreboard(Turtle):
@@ -10,7 +12,7 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 20, 'normal'))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def add_score(self):
         self.score += 1
@@ -18,4 +20,8 @@ class Scoreboard(Turtle):
 
     def refresh(self):
         self.clear()
-        self.write(f"Score: {self.score}", align="center", font=('Arial', 20, 'normal'))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
