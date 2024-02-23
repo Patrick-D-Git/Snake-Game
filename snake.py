aaa287051e084cb7fc27da19519394fbf33c3c83
Patrick D <patrick.montejo@gmail.com>
2024-02-22 22:10:17 -0500

    added condition when the head collides with tail

--- snake.py
+++ snake.py
@@ -12,11 +12,17 @@ class Snake:
 
     def create_snake(self):
         for position in STARTING_POSITION:
-            new_segment = Turtle("square")
-            new_segment.color("white")
-            new_segment.penup()
-            new_segment.goto(position)
-            self.segments.append(new_segment)
+            self.add_segment(position)
+
+    def add_segment(self, position):
+        new_segment = Turtle("square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
+
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -44,4 +50,3 @@ class Snake:
 
         if self.head.heading() != 180.0:
             self.head.setheading(0)
-
