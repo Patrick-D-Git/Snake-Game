e942ac6e5a4b67275edf3eba7fd3c95c2091fcb4
Patrick D <patrick.montejo@gmail.com>
2024-02-26 21:42:20 -0500

    added a high score feature

--- scoreboard.py
+++ scoreboard.py
@@ -12,7 +12,8 @@ class Scoreboard(Turtle):
         self.penup()
         self.goto(0, 270)
         self.score = 0
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.high_score = 0
+        self.refresh()
 
     def add_score(self):
         self.score += 1
@@ -20,8 +21,14 @@ class Scoreboard(Turtle):
 
     def refresh(self):
         self.clear()
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.write(f"Score: {self.score} High Score: {self.high_score}", align=ALIGNMENT, font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset_highscore(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.refresh()
+
+    # def game_over(self):
+    #     self.goto(0, 0)
+    #     self.write(f"GAME OVER", align=ALIGNMENT, font=FONT)
