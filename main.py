37fc8e0ff90743f1e64d6ac4de700413bad516c7
Patrick D <patrick.montejo@gmail.com>
2024-02-26 21:51:32 -0500

    added a reset function for snake

--- main.py
+++ main.py
@@ -37,10 +37,12 @@ while game_is_on:
     #  collision with wall
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         scoreboard.reset_highscore()
+        snake.reset()
 
     #  collision with tail
     for segment in snake.segments[1:]:
         if snake.head.distance(segment) < 10:
             scoreboard.reset_highscore()
+            snake.reset()
 
 screen.exitonclick()
